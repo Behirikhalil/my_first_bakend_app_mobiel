1c3b12709ca4bbb425f9dc90c3d4a979a5a1d9b1
Khalil Behiri <khalilbehiri@example.com>
2025-04-24 22:22:30 +0100

    first commit

--- database.js
+++ database.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose'); 
 async function connectDB() {
     try {
-        await mongoose.connect('mongodb://localhost:27017/testdata');
+        await mongoose.connect(process.env.MONGODB_URI || 'mongodb://mongo:27017/yourDB', {
+         useNewUrlParser: true,
+         useUnifiedTopology: true,
+         serverSelectionTimeoutMS: 5000
+        });
         console.log('✅ Connected to MongoDB');
     } catch (err) {
         console.error('❌ Error connecting to MongoDB:', err);
